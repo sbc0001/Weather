78f7ef5c3106a4c609508af167ef4d47d01228e2
sbc0001 <sbc0001>
2024-11-08 15:42:36 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import WeatherPage from './component/WeatherPage';
+import Home from './component/Home';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>지역 날씨 정보</h1>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/weather/:region/:city' element={<WeatherPage/>}/>
+      </Routes>
     </div>
   );
 }
