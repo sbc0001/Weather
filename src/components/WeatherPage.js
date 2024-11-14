a8e5a4477f2e0ef15dca8c79c1b2e0500ffcd53e
sbc0001 <sbc0001>
2024-11-14 15:58:07 +0900

    Update Home and WeatherPage to show city weather directly

--- src/components/WeatherPage.js
+++ src/components/WeatherPage.js
@@ -1,7 +1,9 @@
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import {useParams, useNavigate} from 'react-router-dom';
 import styled from 'styled-components';
+
+const Button=styled.button`padding:5px 10px; font-size:14px; cursor:pointer; background-color:#ccc; border:none; border-radius:7px; &hover{background-color:#e69;}`;
 const Container = styled.div`
 text-align:center;
 width:300px;
@@ -24,29 +26,31 @@ animation:spin 2.5s linear infinite;
     100%{ transform:rotate(360deg)}
 }
 `;
-const cityMapping = {
-'서울':'Seoul',
-'강남':'Gangnam',
-'인천':'Incheon',
-'경기':'Gyeonggi',
-'수원':'Suwon-si',
-'성남':'Seongnam-si',
-'고양':'Goyang-si',
-'부산':'Busan',
-'서면':'Seomyeon',
-'대구':'Daegu',
-'광주':'Gwangju',
-'대전':'Daejeon',
-'울산':'Ulsan',
-'세종':'Sejong',
-'강릉':'Gangerung',
+const cityMapping={
+    '서울':'Seoul',
+    '인천':'Incheon',
+    '수원':'Suwon-si',
+    '성남':'Seongnam-si',
+    '고양':'Goyang-si',
+    '부산':'Busan',
+    '대구':'Daegu',
+    '광주':'Gwangju',
+    '대전':'Daejeon',
+    '울산':'Ulsan',
+    '세종':'Sejong',
+    '강릉':'Gangneung'
 }
-function WeatherPage({region, city}){
-const [weatherData, setWeatherData] = useState(null);
+
+function WeatherPage(){
+    const navigate=useNavigate();
+    const {region, city}=useParams();
+    const [weatherData, setWeatherData] = useState(null);
+    const [error, setError] = useState(null);
+
 useEffect(()=>{
     const fetchWeather = async () =>{
     const apiKey = '76884e5e22c3fc52ebb0258eff92d74b';
-    const mappedCity = cityMapping[city] || city;
+    const mappedCity=cityMapping[city] || city;
     try{
         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
         params:{
@@ -58,36 +62,30 @@ useEffect(()=>{
         });
         setWeatherData(response.data);
     }catch (error) {
-        if (error.response) {
-            alert(error.response.data.message || "서버에서 오류가 발생했습니다.");
-        } else if (error.request) {
-            alert("서버에 연결할 수 없습니다. 네트워크를 확인하세요.");
-        } else {
-            alert("오류 발생: " + error.message);
-        }
+        setError("날씨 정보를 불러오는 중 오류가 발생했습니다.")
     }
 };
     fetchWeather();
-}, [city])
+}, [city]);
+
 return(
     <Container>
-    <h1>{`${region}지역 - ${city}시의 날씨 정보`}</h1>
-    {weatherData ? (
-        <div>
-        <h2>{weatherData.weather[0].description}</h2>
-        <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt={weatherData.weather[0].description}/>
-        <p><strong>{new Date().toLocaleDateString()}</strong></p>
-        <p>온도 : {weatherData.main.temp}&deg;C </p>
-        <p>체감온도 : {weatherData.main.feels_like}°C </p>
-        <p>풍속 : {weatherData.wind.speed} m/s</p>
-        </div>
-    ) : (
-        <div>
-        <LoadingSpinner />
-        <p>날씨 정보를 불러오는 중입니다...</p>
-        </div>
-    )}
+        {error ? (
+            <p>{error}</p>
+        ) : weatherData ? (
+            <>
+                <h2>{`${region} - ${city} 날씨 정보`}</h2>
+                <p>날씨: {weatherData.weather[0].description}</p>
+                <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt={weatherData.weather[0].description}/>
+                <p>온도: {weatherData.main.temp}°C</p>
+                <p>체감온도: {weatherData.main.feels_like}°C</p>
+                <p>풍속: {weatherData.wind.speed} m/s</p>
+            </>
+        ) : (
+            <LoadingSpinner />
+        )}
+        <Button onClick={() => navigate(-1)}>뒤로가기</Button>
     </Container>
-)
+);
 }
 export default WeatherPage;
